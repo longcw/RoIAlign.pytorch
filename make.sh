d53f59e50e3ead47188e376f951f86bf250b6db8
longc-ubuntu <longch1024@gmail.com>
2017-12-07 11:25:15 +0800

    update path in mask.sh

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 CUDA_PATH=/usr/local/cuda
 
 echo "Compiling crop_and_resize kernels by nvcc..."
-cd crop_and_resize/src/cuda
+cd roi_align/src/cuda
 $CUDA_PATH/bin/nvcc -c -o crop_and_resize_kernel.cu.o crop_and_resize_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
 
 cd ../../
