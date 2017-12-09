3c47d73e0ccfa92ecbba22c99b0d0b614533bed5
longc-ubuntu <longch1024@gmail.com>
2017-12-09 17:37:10 +0800

    add parameter for transform_fpcoor

--- roi_align/roi_align.py
+++ roi_align/roi_align.py
@@ -6,12 +6,13 @@ from crop_and_resize import CropAndResizeFunction, CropAndResize
 
 class RoIAlign(nn.Module):
 
-    def __init__(self, crop_height, crop_width, extrapolation_value=0):
+    def __init__(self, crop_height, crop_width, extrapolation_value=0, transform_fpcoor=True):
         super(RoIAlign, self).__init__()
 
         self.crop_height = crop_height
         self.crop_width = crop_width
         self.extrapolation_value = extrapolation_value
+        self.transform_fpcoor = transform_fpcoor
 
     def forward(self, featuremap, boxes, box_ind):
         """
@@ -23,17 +24,21 @@ class RoIAlign(nn.Module):
         :return: MxCxoHxoW
         """
         x1, y1, x2, y2 = torch.split(boxes, 1, dim=1)
-
-        spacing_w = (x2 - x1) / float(self.crop_width)
-        spacing_h = (y2 - y1) / float(self.crop_height)
-
         image_height, image_width = featuremap.size()[2:4]
-        nx0 = (x1 + spacing_w / 2 - 0.5) / float(image_width - 1)
-        ny0 = (y1 + spacing_h / 2 - 0.5) / float(image_height - 1)
 
-        nw = spacing_w * float(self.crop_width - 1) / float(image_width - 1)
-        nh = spacing_w * float(self.crop_height - 1) / float(image_height - 1)
+        if self.transform_fpcoor:
+            spacing_w = (x2 - x1) / float(self.crop_width)
+            spacing_h = (y2 - y1) / float(self.crop_height)
 
-        boxes = torch.cat((ny0, nx0, ny0 + nh, nx0 + nw), 1)
+            nx0 = (x1 + spacing_w / 2 - 0.5) / float(image_width - 1)
+            ny0 = (y1 + spacing_h / 2 - 0.5) / float(image_height - 1)
+            nw = spacing_w * float(self.crop_width - 1) / float(image_width - 1)
+            nh = spacing_w * float(self.crop_height - 1) / float(image_height - 1)
 
+            boxes = torch.cat((ny0, nx0, ny0 + nh, nx0 + nw), 1)
+        else:
+            boxes = torch.cat((y1, x1, y2, x2), 1)
+
+        boxes = boxes.detach().contiguous()
+        box_ind = box_ind.detach()
         return CropAndResizeFunction(self.crop_height, self.crop_width, self.extrapolation_value)(featuremap, boxes, box_ind)
