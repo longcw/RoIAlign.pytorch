8b5aeb1b02941a5d7a7755a3388f3844a90d0cfc
longc-ubuntu <longch1024@gmail.com>
2018-01-19 10:30:26 +0800

    update for py3

--- roi_align/crop_and_resize.py
+++ roi_align/crop_and_resize.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Function
 
-from _ext import crop_and_resize as _backend
+from ._ext import crop_and_resize as _backend
 
 
 class CropAndResizeFunction(Function):
--- roi_align/roi_align.py
+++ roi_align/roi_align.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 
-from crop_and_resize import CropAndResizeFunction, CropAndResize
+from .crop_and_resize import CropAndResizeFunction, CropAndResize
 
 
 class RoIAlign(nn.Module):
